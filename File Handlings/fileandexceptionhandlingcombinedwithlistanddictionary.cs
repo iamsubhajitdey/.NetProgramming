f71829ce7b677d06120d65b195d9875481110a07
iamsubhajitdey <subhajit18892@gmail.com>
2020-07-17 19:26:50 +0530

    add .net programms for files and string handling

---
+++ File Handlings/fileandexceptionhandlingcombinedwithlistanddictionary.cs
@@ -0,0 +1,138 @@
+﻿// File and Exception Handling combined with List and Dictionary.
+// Carry out the following operations:
+// 1. Two files Players.txt and JerseyDetails.txt will be provided.
+// • Players: list of player names selected in final squad.
+// • JerseyDetails: list of (player name, jersey number) details.
+// 2. Read all contents of Players.txt into a List named Players and throw exception for any numeric data.
+// 3. Read all contents of JerseyDetails.txt into a Dictionary called JerseyDetails and make sure that any exception in data formatting errors in the file is caught in your code.
+// 4. Create a new Dictionary called FinalSquad and this should only contain those players that have matching names in Players and JerseyDetails.
+// 5. Output the contents of FinalSquad to a new file called FinalSquad.txt in the same format as JerseyDetails.txt. This output should be sorted by value of jersey number.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace fileshandling
+{
+    public class Program
+    {
+        public List<String> Players;
+        public Dictionary<String, int> JerseyDetails;
+
+        public void GetPlayersDetails(String playername)
+        {
+            foreach (char c in playername)
+            {
+                if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')))
+                {
+                    throw new InvalidOperationException("Input data of player name is invalid.");
+                }
+            }
+
+            Players.Add(playername);
+        }
+
+        public void GetJerseyDetails(String playername, int jerseynumber)
+        {
+            try
+            {
+                foreach (char c in playername)
+                {
+                    if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')))
+                    {
+                        throw new InvalidOperationException("Input data of player name is invalid.");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An Exception has occurred : {0}", e.Message);
+                return;
+            }
+
+            JerseyDetails.Add(playername, jerseynumber);
+
+        }
+
+        public static void Main(string[] args)
+
+        {
+            String path1 = @"Players.txt";
+            String path2 = @"JerseyDetails.txt";
+
+            String[] lines1;
+            String[] lines2;
+
+            lines1 = File.ReadAllLines(path1);
+            lines2 = File.ReadAllLines(path2);
+
+            Program p = new Program();
+
+            /*Read all contents of Players.txt into a List named Players and throw exception for any numeric data.*/
+
+            p.Players = new List<String>();
+
+            for (int i = 0; i < lines1.Length; i++)
+            {
+                p.GetPlayersDetails(lines1[i]);
+            }
+
+            /* Read all contents of JerseyDetails.txt into a Dictionary called JerseyDetails and make 
+               sure that any exception in data formatting errors in the file is caught in your code.*/
+
+            p.JerseyDetails = new Dictionary<string, int>();
+
+            for (int k = 0; k < lines2.Length; k++)
+            {
+                try
+                {
+                    String[] spearator = { "," };
+                    String[] jerseydetailsList = lines2[k].Split(",");
+
+                    string playername = jerseydetailsList[0];
+                    int jerseynumber = Convert.ToInt32(jerseydetailsList[1]);
+                    p.GetJerseyDetails(playername, jerseynumber);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An Exception has occurred : {0}", e.Message);
+                }
+
+            }
+
+            /* Create a new Dictionary called FinalSquad and this should only contain 
+               those players that have matching names in Players and JerseyDetails.*/
+
+            Dictionary<string, int> FinalSquad = new Dictionary<string, int>();
+
+            foreach (var player in p.JerseyDetails)
+            {
+                string key = player.Key;
+                int value = player.Value;
+
+                if (p.Players.Contains(key))
+                {
+                    FinalSquad.Add(key, value);
+                }
+
+            }
+
+            /* Output the contents of FinalSquad to a new file called FinalSquad.txt in the same 
+               format as JerseyDetails.txt. This output should be sorted by value of jersey number.*/
+
+            String FileName = @"FinalSquad.txt";
+            {
+                using(StreamWriter filestr = new StreamWriter(FileName))
+                {
+                    foreach (KeyValuePair<string, int> item in FinalSquad.OrderBy(i => i.Value))
+                    {
+                        filestr.WriteLine(item.Key + "," + item.Value);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Strings/vowelconsonantinstring.cs
@@ -0,0 +1,49 @@
+﻿// 9. Write a program in C# Sharp to count a total number of vowel or consonant in a string. Go to the editor
+// Test Data :
+// Input the string : Welcome to w3resource.com
+// Expected Output :
+
+// The total number of vowel in the string is : 9 
+// The total number of consonant in the string is : 12 
+
+using System;
+
+namespace vowelconsonentinstring
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i, ln, vowel, consonant;
+            string str;
+
+            Console.WriteLine("\n\nCount a total number of vowel or consonant in a sring");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.Write("\n=> Input the string : ");
+            str = Console.ReadLine();
+
+            Console.WriteLine("\n\nExpected Output :");
+            Console.WriteLine("--------------------\n");
+
+            vowel = 0;
+            consonant = 0;
+            ln = str.Length;
+
+            for (i = 0; i < ln; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+                {
+                    vowel++;
+                }
+
+                else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'a' && str[i] <= 'z'))
+                {
+                    consonant++;
+                }
+            }
+
+            Console.WriteLine("=> The total number of vowel in the string is : {0}\n", vowel);
+            Console.WriteLine("=> The total number of consonant in the string is : {0}\n\n", consonant);
+        }
+    }
+}
